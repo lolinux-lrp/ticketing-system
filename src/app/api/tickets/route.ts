aeb16c84a890b50a59df8f1c97fbdf2f778ce212
lolinux-lrp <lolinux.lrp@gmail.com>
2026-07-01 20:34:02 +0530

    Added Initial POST and GET routes for tickets

--- src/app/api/tickets/route.ts
+++ src/app/api/tickets/route.ts
@@ -1,1 +1,41 @@
-export async function GET() { return new Response('tickets'); }
+import { NextResponse } from "next/server"
+import {prisma} from "@/lib/prisma"
+
+export async function POST(req: Request){
+    try{
+
+        const body = await req.json();
+        const {createdById,description,title} = body;
+
+        if(!createdById || !description || !title){
+            return NextResponse.json({error: "All fields necessary for making a ticket"},{status: 400});
+        }
+        
+        const newTicket = await prisma.ticket.create({
+            data:{
+                createdById,
+                description,
+                title
+            }
+        });
+
+        return NextResponse.json(newTicket,{status: 201});
+    }catch{
+        return NextResponse.json({error: "Failed to create ticket"}, {status: 500})
+    }
+}
+
+export async function GET(){
+    try{
+        const tickets = await prisma.ticket.findMany({
+            orderBy: {
+                createdAt: 'desc'
+            }
+        });
+
+        return NextResponse.json(tickets,{status: 200});
+    }catch(error){
+        console.log("Error fetching tickets:", error)
+        return NextResponse.json({error: "Failed to fetch tickets"}, {status: 500})
+    }
+}
