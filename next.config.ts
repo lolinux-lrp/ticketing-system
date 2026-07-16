715736955cb450c5c8db4d3d71c023da384ebd17
lolinux-lrp <lolinux.lrp@gmail.com>
2026-07-16 11:05:15 +0530

    fix: allow Next.js dev server access from local network

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  allowedDevOrigins: ['192.168.17.127'],
+  allowedDevOrigins: ['192.168.17.127', '192.168.19.98'],
 };
 
 export default nextConfig;
